aff3e819e1a3f1bce87e80b621bd31dc905ee3f2
范磊鑫 <1044978714@qq.com>
2019-08-07 16:09:01 +0800

    modify

---
+++ console/js/alert.js
@@ -0,0 +1,15 @@
+function taoLu(){
+    alert("你猜一会会发生什么");
+    alert("再等等");
+    alert("也许有奇迹");
+}
+
+
+alert("你怎么打开这个网页");
+alert("我知道你在好奇");
+
+taoLu();
+taoLu();
+taoLu();
+
+alert("这是最后一句了，88");
