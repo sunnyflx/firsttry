90bd8dcf695aee3372c9ee5c6919c598c7da8e86
范磊鑫 <1044978714@qq.com>
2019-08-13 22:33:49 +0800

    modify

--- 004Mouse/fingerGuess/main.js
+++ 004Mouse/fingerGuess/main.js
@@ -1,18 +1,21 @@
-let r,score=0,grade=1,lastHumanChoice;
-let isComputerWin=false,lastComputerChoice;
+let r,score=0,grade=3,lastHumanChoice;
+let lastWinner="noOne",lastComputerChoice="rock",humanChoice;
 function rock() {
     document.getElementById("mychoice").innerHTML="<img src=\"images/rock.png\" alt/>";
+    humanChoice="rock";
     judge("rock");
     lastHumanChoice="rock";
 }
 function scissors() {
     document.getElementById("mychoice").innerHTML="<img src=\"images/scissors.png\" alt/>";
+    humanChoice="scissors";
     judge("scissors");
     lastHumanChoice="scissors";
 }
 function paper() {
     document.getElementById("mychoice").innerHTML="<img src=\"images/paper.png\" alt/>";
-    judge("paper");   
+    humanChoice="paper";
+    judge("paper");
     lastHumanChoice="paper";
 }
 function judge(mychoice) {
@@ -28,61 +31,70 @@ function judge(mychoice) {
         computerResult=winnerAgain();
         lastComputerChoice=computerResult;
     }
-    else{
-        computerResult=computerChoice();
+    else if (grade===4){
+        computerResult=loseChange();
+        lastComputerChoice=computerResult;
+    }
+    else if (grade===5){
+        computerResult=loveRock();
+    }
+    else if (grade===6){
+        computerResult=followHeart();
+    }
+    else if (grade===7){
+        computerResult=cheatingMan();
+        humanChoice=computerResult;
     }
     if (mychoice === "rock") {
         if (computerResult === "rock") {
-            isComputerWin=false;
+            lastWinner="noOne";
         }
         else if (computerResult ==="scissors") {
-            isComputerWin=false;
-            score+=1
+            lastWinner="human";
+            score+=1;
         }
         else if (computerResult==="paper") {
-            isComputerWin=true;
-            score-=1       
+            lastWinner="com";
+            score-=1;
         }
     }
     else if (mychoice === "scissors") {
         if (computerResult ==="rock") {
-            isComputerWin=true;
             score-=1;
+            lastWinner="com";
         }
         else if (computerResult ==="scissors") {
-            isComputerWin=false;
+            lastWinner="noOne";
         }
         else if (computerResult==="paper") {
-            isComputerWin=false;
             score+=1;
+            lastWinner="human";
         }
     }
     else {
         if (computerResult === "rock") {
-            isComputerWin=false;
+            lastWinner="human";
             score+=1;
         }
         else if (computerResult ==="scissors") {
-            isComputerWin=true;
             score-=1;
+            lastWinner="com";
         }
         else if (computerResult==="paper") {
-            isComputerWin=false;
+            lastWinner="noOne";
         }
     }
-    if (score>=6){
+    if (score>=5){
         score=0;
         grade+=1;
     }
     document.getElementById("result").innerHTML="第"+grade+"关"+"积分："+score;
-    if (grade>=10){
+    if (grade>7){
         document.getElementById("result").innerHTML="NB,通关了";
     }
 }
-function go() {
-    r = 3*Math.random();
-}
 function computerChoice() {
+    r = 3*Math.random();
     if (r<1){
         document.getElementById("computerChoice").innerHTML="<img src=\"images/rock.png\" alt/>";
         return"rock";
@@ -107,12 +119,108 @@ function learnFromHuman() {
     return lastHumanChoice;
 }
 function winnerAgain() {
-    document.getElementById("computerName").innerHTML="赢了还要";
-    let temp =computerChoice();
-    if (isComputerWin===true){
+    document.getElementById("computerName").innerHTML="赢了还出";
+    if (lastWinner==="com"){
         document.getElementById("computerChoice").innerHTML="<img src=\"images/"+lastComputerChoice+".png\" alt/>";
         return lastComputerChoice;
     }
-    document.getElementById("computerChoice").innerHTML="<img src=\"images/"+temp+".png\" alt/>";
+    let temp =computerChoice();
+    document.getElementById("computerChoice").innerHTML="<img src=\"images/"+ temp +".png\" alt/>";
     return temp;
+}
+
+function loseChange(){
+    document.getElementById("computerName").innerHTML="输了就换";
+    if(lastWinner==="human"){
+        let temp = getResultExclude(lastComputerChoice);
+        document.getElementById("computerChoice").innerHTML="<img src='images/"+temp+".png' alt/>";
+        return temp;
+    }
+    let temp = computerChoice();
+    document.getElementById("computerChoice").innerHTML="<img src='images/"+ temp +".png' alt/>";
+    return temp;
+}
+
+//递归，
+function getResultExclude(exclusion){
+    let temp = computerChoice();
+    if(temp===exclusion){
+        return getResultExclude(exclusion);
+    }
+    else{
+        return temp;
+    }
+}
+
+function loveRock() {
+    r = 3*Math.random();
+    let temp;
+    if (r<0.8){
+        document.getElementById("computerChoice").innerHTML="<img src=\"images/rock.png\" alt/>";
+        temp="rock";
+    }
+    else if (r<1.8){
+        document.getElementById("computerChoice").innerHTML="<img src=\"images/scissors.png\" alt/>";
+        temp="scissors";
+    }
+    else{
+        document.getElementById("computerChoice").innerHTML="<img src=\"images/paper.png\" alt/>";
+        temp="paper";
+    }
+    document.getElementById("computerName").innerHTML="爱石先生";
+    document.getElementById("computerChoice").innerHTML="<img src='images/"+ temp +".png' alt/>";
+    return temp;
+}
+
+
+function followHeart() {
+    r = 3*Math.random();
+    let temp;
+    if (r<1){
+        document.getElementById("computerChoice").innerHTML="<img src=\"images/rock.png\" alt/>";
+        temp="rock";
+    }
+    else if (r<2){
+        document.getElementById("computerChoice").innerHTML="<img src=\"images/scissors.png\" alt/>";
+        temp="scissors";
+    }
+    else{
+        document.getElementById("computerChoice").innerHTML="<img src=\"images/paper.png\" alt/>";
+        temp="paper";
+    }
+    document.getElementById("computerName").innerHTML="随心所欲";
+    document.getElementById("computerChoice").innerHTML="<img src='images/"+ temp +".png' alt/>";
+    return temp;
+}
+
+function cheatingMan() {
+    document.getElementById("computerName").innerHTML="作弊小子";
+    let r=Math.random();
+    if (humanChoice==="rock"){
+        if (r<0.5){
+            document.getElementById("computerChoice").innerHTML="<img src=\"images/paper.png\" alt/>";
+            return"paper";
+        }
+        else{
+            return computerChoice;
+        }
+    }
+    else if (humanChoice==="scissors"){
+        if (r<0.5){
+            document.getElementById("computerChoice").innerHTML="<img src=\"images/rock.png\" alt/>";
+            return "rock";
+        }
+        else{
+            return computerChoice;
+        }
+    }
+    else{
+        if (r<0.5){
+            document.getElementById("computerChoice").innerHTML="<img src=\"images/scissors.png\" alt/>";
+            return "scissors";
+        }
+        else{
+            return computerChoice;
+        }
+    }
 }
